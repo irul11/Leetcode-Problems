424fd501ab8c5741657cd9f3d006ddf8c9f099ad
Irul11 <amtaqiy11@gmail.com>
2024-06-23 07:58:58 +0700

    Solution no.1438 (Daily Challange 23 June 2024)

---
+++ Python/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        maxd, mind = deque(), deque()
+        i = 0
+
+        for j, num in enumerate(nums):
+            while maxd and num > maxd[-1]:
+                maxd.pop()
+            while mind and num < mind[-1]:
+                mind.pop()
+            
+            maxd.append(num)
+            mind.append(num)
+
+            if maxd[0] - mind[0] > limit:
+                if maxd[0] == nums[i]:
+                    maxd.popleft()
+                if mind[0] == nums[i]:
+                    mind.popleft()
+                i += 1
+
+        return len(nums) - i
