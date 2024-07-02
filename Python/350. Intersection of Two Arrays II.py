668c55c31a2020457aea7a3b8b7da978164b4450
Irul11 <amtaqiy11@gmail.com>
2024-07-02 09:08:59 +0700

    Update solution no.350

--- Python/350. Intersection of Two Arrays II.py
+++ Python/350. Intersection of Two Arrays II.py
@@ -1,18 +1,12 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         ans = []
-        if len(nums1) <= len(nums2):
-            counter = Counter(nums1)
-            for n in nums2:
-                if n in counter and counter[n] > 0:
-                    ans.append(n)
-                    counter[n] -= 1
-        else:
-            counter = Counter(nums2)
-            for n in nums1:
-                if n in counter and counter[n] > 0:
-                    ans.append(n)
-                    counter[n] -= 1
+        counter = Counter(nums1)
         
+        for n in nums2:
+            if n in counter and counter[n] > 0:
+                ans.append(n)
+                counter[n] -= 1
+
         return ans
         
