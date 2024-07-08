6dd94864f5786b25aa68e16da5b319ecb939807c
Irul11 <amtaqiy11@gmail.com>
2024-07-08 13:54:20 +0700

    Solution no.1823 (Daily Challange 8 July 2024)

---
+++ Python/1823. Find the Winner of the Circular Game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        winner = 0
+
+        for i in range(1, n+1):
+            winner = (winner + k) % i
+            # print(winner)
+        
+        return winner + 1
